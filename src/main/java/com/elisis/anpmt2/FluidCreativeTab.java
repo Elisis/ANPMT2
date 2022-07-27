3f886d0d2f081c2be0733da78eefd49e70e6c663
unknown <gtandemmodding@gmail.com>
2022-07-27 16:20:37 +1000

    Add search bar, sorting to fluid creative tab

--- src/main/java/com/elisis/anpmt2/FluidCreativeTab.java
+++ src/main/java/com/elisis/anpmt2/FluidCreativeTab.java
@@ -2,9 +2,12 @@ package com.elisis.anpmt2;
 
 import com.elisis.anpmt2.enums.Materials;
 import com.elisis.anpmt2.util.MaterialUtils;
+import com.elisis.anpmt2.util.TabAlphaComparator;
+import com.elisis.anpmt2.util.TabNumberComparator;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -19,5 +22,23 @@ public class FluidCreativeTab extends CreativeTabs {
 	public ItemStack getTabIconItem() {
 		return new ItemStack(MaterialUtils.getItem(Materials.Magnesium, "ingot"));
 	}
+	
+	@Override
+    public void displayAllRelevantItems(NonNullList<ItemStack> items) {
+        
+		
+		
+		
+		
+		super.displayAllRelevantItems(items);
+		
+        items.sort(new TabAlphaComparator().thenComparing(new TabNumberComparator())); //More hacky shite. Sorts by alpha, then numerically
+    }
+	
+	@Override
+	public boolean hasSearchBar()
+    {
+        return true;
+    }
 		
 }
